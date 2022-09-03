d9a957d8608de6acec937ff62be849ae3938ca8a
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-09-03 13:21:38 +0200

    Update main_max_min_per_timestep.py

--- backend/main_max_min_per_timestep.py
+++ backend/main_max_min_per_timestep.py
@@ -1,30 +1,36 @@
 import yaml
 import simulation
-from container import container
 import matplotlib.pyplot as plt
 import numpy as np
-
+from datetime import datetime, timedelta
 
 def main():
     with open("config_simulation.yaml", encoding="utf-8") as f:
         config_simulation = yaml.safe_load(f)
-    container.config_simulation = config_simulation
-    container.sims = simulation.from_config(**config_simulation)
+    config_simulation = config_simulation
+    sims = simulation.from_config(**config_simulation)
 
-    ticks = len(container.sims[0].values)
+    sim0 = sims[0]
     mins = []
     maxs = []
-    timesteps = container.sims[0].times
-    for tick in range(0, ticks):
-        values = [simulation.Simulation.get_price(sim.values[tick]) for sim in container.sims]
+    time0 = sim0.times[0]
+    party_start = datetime(time0.year, time0.month, time0.day, 20, 30)
+    party_end = datetime(time0.year, time0.month, time0.day, 3, 0) + timedelta(days=1)
+    start_tick = int((party_start - time0).total_seconds() / (sim0.tick_interval / 1000)) + 1
+    end_tick = int((party_end - time0).total_seconds() / (sim0.tick_interval / 1000)) + 1
+    every_n_ticks = 300
+    for tick in range(start_tick, end_tick + 1, every_n_ticks):
+        values = [simulation.Simulation.get_price(sim.values[tick]) for sim in sims]
         mins.append(min(values))
         maxs.append(max(values))
         # print(f"{timesteps[tick]}: {min(values):.2f}-{max(values):.2f}")
 
     # creat plot
-    plt.fill_between(range(len(timesteps)), np.array(mins) - 0.25, np.array(maxs) + 0.25, alpha=0.5)
-    x = range(0, len(timesteps), int(7200 / (container.sims[0].tick_interval / 1000)) + 1)
-    plt.xticks(x, [timesteps[i].time().strftime("%H:%M") for i in x])
+    plt.plot(range(len(mins)), mins)
+    plt.plot(range(len(mins)), maxs)
+    xticks_interval = 5
+    x = list(range(start_tick, end_tick + 1, every_n_ticks * xticks_interval))
+    plt.xticks(range(0, len(mins), xticks_interval), [sim0.times[i].time().strftime("%H:%M") for i in x])
     plt.show()
 
 
